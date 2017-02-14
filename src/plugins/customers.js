6528b934218ba1d36608af401bb8fed043443367
Tanner <tanner.crockett@gmail.com>
2017-02-14 14:59:41 -0700

    password update

--- src/plugins/customers.js
+++ src/plugins/customers.js
@@ -1,7 +1,20 @@
 import {collection} from '../db';
 import Joi from 'joi';
 import Boom from 'boom';
+import Bcrypt from 'bcryptjs';
 
+const encryptPassword = async(password) => {
+
+  return await new Promise((resolve, reject) => {
+    return Bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(hash);
+    })
+
+  })
+};
 const plugin = (server, options, next) => {
 
   server.route({
@@ -14,19 +27,18 @@ const plugin = (server, options, next) => {
           firstName: Joi.string().required(),
           lastName: Joi.string().required(),
           email: Joi.string().email().lowercase().required(),
-          password: Joi.string().min(7).required(),
-          passwordConfirmation: Joi.string().min(7).required()
+          password: Joi.string().min(7).required()
         }
       }
     },
     handler: {
       async: async(request, reply) => {
-        const customer = request.payload;
-        if(customer.password !== customer.passwordConfirmation) {
-          return reply(Boom.badRequest('Passwords do not match', customer.password));
-        }
+        const {firstName, lastName, email, password} = request.payload;
+
+        const hashpassword = await encryptPassword(password);
+
         const customers = await collection('customers');
-        const result = await customers.insertOne(customer);
+        const result = await customers.insertOne({firstName, lastName, email, hashpassword});
         if(result.insertedCount === 1){
           return reply(201, {message: 'User created successfully!'})
         }
