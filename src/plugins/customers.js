1e1eff6f36ceb7274364a96e22fbdf270a3c565c
Tanner <tanner.crockett@gmail.com>
2017-02-13 17:16:49 -0700

    Customer API

--- src/plugins/customers.js
+++ src/plugins/customers.js
@@ -1,5 +1,6 @@
 import {collection} from '../db';
 import Joi from 'joi';
+import Boom from 'boom';
 
 const plugin = (server, options, next) => {
 
@@ -10,17 +11,31 @@ const plugin = (server, options, next) => {
       tags: ['api', 'v1'],
       validate: {
         payload: {
-
+          firstName: Joi.string().required(),
+          lastName: Joi.string().required(),
+          email: Joi.string().email().lowercase().required(),
+          password: Joi.string().min(7).required(),
+          passwordConfirmation: Joi.string().min(7).required()
         }
       }
     },
     handler: {
       async: async(request, reply) => {
-        return reply({});
+        const customer = request.payload;
+        if(customer.password !== customer.passwordConfirmation) {
+          return reply(Boom.badRequest('Passwords do not match', customer.password));
+        }
+        const customers = await collection('customers');
+        const result = await customers.insertOne(customer);
+        if(result.insertedCount === 1){
+          return reply(201, {message: 'User created successfully!'})
+        }
+        return await reply(Boom.badImplementation('An error occurred'));
+
       }
     }
 
-  })
+  });
   next();
 };
 
